98fe016447c31e5b8665ed07366dbbed0cd6d3e1
shundev23 <140191667+shundev23@users.noreply.github.com>
2024-12-08 12:33:52 +0900

    add arrow func

--- react-guide-material/03_js_basic/020_arrow_function/start/main.js
+++ react-guide-material/03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,27 @@
 function fn(number) {
   return number * 2;
 }
+
+// arrow関数の記載方法
+/* const fn = (number) => {
+  return number * 2;
+}
+*/
+
+console.log("fn(2):",fn(2));
+
+// 省略前
+const fnArrow = (number) => {
+  console.log(number);
+  return number * 2;
+}
+
+// 省略後
+// const fnArrow = number => number * 2;
+
+console.log("fnArrow(2):",fnArrow(2));
+
+// obj形式の戻り値
+const fnArrowObj = number => ({ result: number * 2 })
+
+console.log("fnArrowObj:",fnArrowObj(2));
