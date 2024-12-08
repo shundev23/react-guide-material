e00f012392d7c4fd0aafa9019cae0e6de85169b3
shundev23 <140191667+shundev23@users.noreply.github.com>
2024-12-08 15:28:05 +0900

    upd random func

--- react-guide-material/03_js_basic/100_async_await/start/main.js
+++ react-guide-material/03_js_basic/100_async_await/start/main.js
@@ -1,17 +1,27 @@
-// 非同期処理（Promise）
+// 非同期処理（await/async）
 let a = 0;
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        a = 1;
-        resolve(a)
-    }, 2000);
-}).then((b) => {
-    console.log(b);
-    return b;
-}).then((b) => {
-    console.log(b);
-}).catch((c) => {
-    console.log('catchが実行', c)
+init();
+const btn_click = document.querySelector('button');
+async function init(){
+    try{
+        const result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const value = Math.random();
+                document.getElementsByClassName("random-num")[0].textContent = value;
+                if  (value / 2 == 0){
+                    console.log(`${value}は偶数です`);
+                }
+                else{
+                    console.log(`${value}は奇数です`);
+                }
+            }, 2000);
+        })
+        console.log(result);
+    } catch(e) {
+        console.log('catchが実行', e)
+    }
+}
+btn_click.addEventListener('click', () => {
+    init();
 })
-
