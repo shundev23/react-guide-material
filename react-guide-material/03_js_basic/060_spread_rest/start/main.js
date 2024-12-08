3c25ca29256bec285e7bc1f799f3a4142a59ff6b
shundev23 <140191667+shundev23@users.noreply.github.com>
2024-12-08 14:26:58 +0900

    Update main.js

--- react-guide-material/03_js_basic/060_spread_rest/start/main.js
+++ react-guide-material/03_js_basic/060_spread_rest/start/main.js
@@ -1,25 +1,29 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
-console.log(result);
+var max = Math.max(...nums);
+var min = Math.min(...nums); 
+console.log(max);
+console.log(min);
 
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
-console.log(newArr2);
+let newArr = [...arr1, 10, ...arr2];
+let newArr1 = arr1;
+// newArr.push(4)
+console.log(newArr);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
+newObj.name = 'John';
 
-console.log(newObj);
+console.log(obj, newObj);
 
 const restA = (...argA) => console.log(argA);
-restA(1, 3, 4)
+restA(1, 3)
 
-const restB = (n, ...argB) => console.log(argB);
+const restB = (n, ...argB) => console.log(n,argB);
 restB(1, 3, 4)
